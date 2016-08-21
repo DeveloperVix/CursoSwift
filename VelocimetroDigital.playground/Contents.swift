26303af6e703af48717517e0bb67cf9c72f1a88c
DeveloperVix <vicomisterio@hotmail.com>
2016-08-21 00:24:35 -0500

    MiniReto2
    Velocímetro Digital

---
+++ VelocimetroDigital.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+enum Velocidades : Int {
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init(velocidadInicial : Velocidades){
+        self = velocidadInicial
+    }
+}
+
+class Auto{
+    var velocidad : Velocidades
+    
+    init(){
+        velocidad = Velocidades(velocidadInicial : Velocidades.Apagado)
+    }
+    
+    func  cambioDeVelocidad() -> (actual : Int, velocidadEnCadena : String) {
+        if(velocidad == .Apagado){
+            velocidad = .VelocidadBaja
+        }else if(velocidad == .VelocidadBaja){
+            velocidad = .VelocidadMedia
+        }else if(velocidad == .VelocidadMedia){
+            velocidad = .VelocidadAlta
+        }else if(velocidad == .VelocidadAlta){
+            velocidad = .VelocidadMedia
+        }
+        
+        return(velocidad.rawValue, String(velocidad))
+    }
+}
+
+var auto = Auto()
+
+print("\(auto.velocidad.rawValue) \(auto.velocidad)")
+
+for i in 0 ..< 20{
+   print(auto.cambioDeVelocidad())
+}
